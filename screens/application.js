866addaf34a37ea6b28fa6eb04cc40c8797942b8
Pranita <pranita.gawade@udgama.com>
2021-07-05 14:16:47 +0530

    removed apollo

--- screens/application.js
+++ screens/application.js
@@ -5,31 +5,9 @@ import { WebSocketLink } from '@apollo/client/link/ws';
 import JobFromEmployer from '../components/applicationQuery';
 
 
-export default function ApplicantScreen({navigation}) {
-  const wsLink = new WebSocketLink({
-    uri: 'wss://team-c.hasura.app/v1/graphql',
-    options: {
-      reconnect: true,
-      connectionParams: {
-        headers: {
-          'Content-Type': 'application/json',
-          'x-hasura-admin-secret': 'PcEURINYV1b1OVT8z0l0jsAvMb8Wkt67rJHtTPt8oKcTaLFeLwPAKPIJfe0S7V6g',
-        }
-      }
-    },
-  });
-  
-  const client = new ApolloClient({
-    link: wsLink,
-    cache: new InMemoryCache()
-  });
-  
+export default function ApplicantScreen({navigation}) {  
     return(
-        <ApolloProvider>
-            
           <JobFromEmployer 
           navigation={navigation}/> 
-          
-        </ApolloProvider>
       )
 }
